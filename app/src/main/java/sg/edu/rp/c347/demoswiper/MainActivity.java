a94ad0951dd37a76293cf196af043c253c366cf0
Kiraow188 <17010368@myrp.edu.sg>
2019-07-16 11:35:37 +0800

    Section C

--- app/src/main/java/sg/edu/rp/c347/demoswiper/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/demoswiper/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -14,12 +16,38 @@ public class MainActivity extends AppCompatActivity {
     MyFragmentPagerAdapter adapter;
     ViewPager vPager;
 
+    Button btnBack, btnNext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         vPager = findViewById(R.id.viewpager1);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnNext = findViewById(R.id.btnNext);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int max = vPager.getChildCount();
+                if (vPager.getCurrentItem() < max-1){
+                    int nextpage = vPager.getCurrentItem() + 1;
+                    vPager.setCurrentItem(nextpage, true);
+                }
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (vPager.getCurrentItem() > 0){
+                    int previouspage = vPager.getCurrentItem() - 1;
+                    vPager.setCurrentItem(previouspage, true);
+                }
+            }
+        });
 
         FragmentManager fm = getSupportFragmentManager();
 
